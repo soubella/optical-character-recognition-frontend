b520b570512c56e60b35e0969de54e483f6133e1
moussaidOualid <moussaid.oualid@gmail.com>
2020-01-04 21:40:12 +0100

    config home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient , HttpParams} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'optical-character-recognition-frontend';
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    // Making the HTTP Request
+    this.http
+      .get('')
+      .subscribe(data => {
+        console.log(data['test']);
+
+      })
+  }
 }
