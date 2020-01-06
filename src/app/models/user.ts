de3bd8a6247ad490699a97ea0a6eb5f721b41eb6
moussaidOualid <moussaid.oualid@gmail.com>
2020-01-06 15:21:31 +0100

    post user registration data added

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id : number;
+    firstName : String;
+    lastName : String;
+    email : String;
+    password : String;
+    state : boolean;
+
+    role :any ;
+
+     entreprise : any;
+}
